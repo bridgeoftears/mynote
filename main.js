1616feacde293199629714d8d6b052c6a78eaae0
bridgeoftears <2571714054@qq.com>
2025-03-01 20:41:50 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+import App from './App'
+
+// #ifndef VUE3
+import Vue from 'vue'
+Vue.config.productionTip = false
+App.mpType = 'app'
+const app = new Vue({
+    ...App
+})
+app.$mount()
+// #endif
+
+// #ifdef VUE3
+import { createSSRApp } from 'vue'
+export function createApp() {
+  const app = createSSRApp(App)
+  return {
+    app
+  }
+}
+// #endif
+
+var APITOKEN = 'myuniapp';
+Vue.prototype.apiServer = 'http://127.0.0.1/api.php?token=' + APITOKEN + '&action=';
+
+Vue.prototype.checkLogin = function(backpage) { //添加全局变量
+	var UID = uni.getStorageSync('UID'); //用户ID
+	if (UID == '') {
+		console.log("没登陆")
+		uni.redirectTo({
+			url: '/pages/login/login?backpage=' + backpage //该页面必须存在于pages.json
+		});
+		return false;
+	} else {
+		console.log("已登陆")
+		return UID;
+	}
+}
+
